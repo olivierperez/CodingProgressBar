d4db0f3e875158a19b9e09eeaefcf98a246a89e6
Olivier PEREZ <olivier@olivierperez.fr>
2024-02-05 21:07:56 +0100

    Fix image loading

--- src/main/kotlin/fr/o80/codingprogress/data/CafeIcons.kt
+++ src/main/kotlin/fr/o80/codingprogress/data/CafeIcons.kt
@@ -1,15 +1,38 @@
 package fr.o80.codingprogress.data
 
 import com.intellij.openapi.util.IconLoader
+import java.awt.Image
 import java.awt.image.BufferedImage
+import javax.swing.ImageIcon
+
 
 object CafeIcons {
-
+    @JvmField
     var HUSKY_LEFT = loadImage("/husky-left.png")
 
+    @JvmField
     var HUSKY_RIGHT = loadImage("/husky-right.png")
 
-    private fun loadImage(filename: String) =
-        IconLoader.toImage(IconLoader.getIcon(filename, CafeIcons::class.java)) as? BufferedImage
+    private fun loadImage(filename: String): BufferedImage {
+        return IconLoader.toImage(ImageIcon(CafeIcons::class.java.getResource(filename)))
+            ?.toBufferedImage()
             ?: error("Cannot load progress bar image from file: $filename")
+    }
+
+    private fun Image.toBufferedImage(): BufferedImage {
+        if (this is BufferedImage) {
+            return this
+        }
+
+        return BufferedImage(
+            this.getWidth(null),
+            this.getHeight(null),
+            BufferedImage.TYPE_INT_ARGB
+        ).apply {
+            createGraphics().apply {
+                drawImage(this@toBufferedImage, 0, 0, null)
+                dispose()
+            }
+        }
+    }
 }
